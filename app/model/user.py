dd9ea5a93fce95df96c71c63a745c7bb865e66f6
tree <2332532718@qq.com>
2019-12-13 18:00:12 +0800

    <bugfix>: <登录token>
    <body>
    
    <footer>

--- app/model/user.py
+++ app/model/user.py
@@ -8,27 +8,29 @@ from .base import BaseModel
 
 
 class User(db.Model, BaseModel):
-    __tablename__ = 'bao_user'
+    __tablename__ = "bao_user"
 
-    id = Column(INTEGER, Sequence('user_id_seq', start=1,
-                                  increment=1), primary_key=True, comment='用户的id')
-    sex = Column(SMALLINT, nullable=True, default=0, comment='0 未设置 1 男性 2 女性')
-    identifier = Column(String(128), nullable=True,
-                        comment='用户的标识符，在某些情况不适合用id，会用此字段')
+    id = Column(
+        INTEGER,
+        Sequence("user_id_seq", start=1, increment=1),
+        primary_key=True,
+        comment="用户的id",
+    )
+    sex = Column(SMALLINT, nullable=True, default=0, comment="0 未设置 1 男性 2 女性")
+    identifier = Column(String(128), nullable=True, comment="用户的标识符，在某些情况不适合用id，会用此字段")
     mobilephone = Column(String(11), nullable=True)
     email = Column(String(64), nullable=True, unique=True)
     password = Column(String(64), nullable=True)
-    status = Column(SMALLINT, nullable=True, default=1,
-                    comment='0 未激活 1 正常 2 异常 3 注销')
+    status = Column(SMALLINT, nullable=True, default=1, comment="0 未激活 1 正常 2 异常 3 注销")
 
     def __init__(
-            self,
-            email: AnyStr,
-            sex: int = 0,
-            mobilephone: Optional[AnyStr] = None,
-            password: Optional[AnyStr] = None,
-            status: int = 1,
-            identifier: Optional[AnyStr] = None
+        self,
+        email: AnyStr,
+        sex: int = 0,
+        mobilephone: Optional[AnyStr] = None,
+        password: Optional[AnyStr] = None,
+        status: int = 1,
+        identifier: Optional[AnyStr] = None,
     ):
 
         self.email = email
@@ -38,23 +40,36 @@ class User(db.Model, BaseModel):
         self.sex = sex
         self.identifier = identifier
 
+    @property
+    def get_cache_key(self) -> AnyStr:
+        return (
+            str(self.identifier)
+            if self.identifier
+            else "sky_user_cache_key_{user_id}".format(user_id=self.id)
+        )
+
 
 class LoginRecordModel(db.Model, BaseModel):
-    __tablename__ = 'bao_login_record'
+    __tablename__ = "bao_login_record"
 
-    record_id = Column(INTEGER, Sequence('login_record_id_seq', start=1,
-                                         increment=1), primary_key=True, comment='用户的登录记录')
+    record_id = Column(
+        INTEGER,
+        Sequence("login_record_id_seq", start=1, increment=1),
+        primary_key=True,
+        comment="用户的登录记录",
+    )
     user_id = Column(INTEGER, nullable=True)
     op_time = Column(String(20), nullable=True)
     op_ip = Column(String(20), nullable=True)
 
     def __init__(
-            self,
-            user_id: int,
-            op_ip: Optional[AnyStr] = None,
-            op_time: Optional[AnyStr] = None
+        self,
+        user_id: int,
+        op_ip: Optional[AnyStr] = None,
+        op_time: Optional[AnyStr] = None,
     ):
         from app.utils import get_unix_time_tuple
+
         self.user_id = user_id
         self.op_time = op_time or get_unix_time_tuple()
         self.op_ip = op_ip
--- app/views/user.py
+++ app/views/user.py
@@ -9,29 +9,33 @@ from app.utils import login_require
 from app.model import User, LoginRecordModel
 import app
 
-api = Blueprint('user', __name__)
-app.fetch_route(api, '/user')
+api = Blueprint("user", __name__)
+app.fetch_route(api, "/user")
 
-@api.route('/register', methods=['POST'])
+
+@api.route("/register", methods=["POST"])
 def register():
     params = parse_params(request)
     email: str = params.get("email")
     password: str = params.get("password")
-    q = session.query(User).filter(User.email == email, User.password==password)
+    q = session.query(User).filter(User.email == email, User.password == password)
     exsist_user = session.query(q.exists())
     if exsist_user:
         return UserError.get_error(error_code=40200)
     user = User(email, password=password)
     session.add(user)
-    payload: Dict[AnyStr, int] = {'user_id': user.id}
+    payload: Dict[AnyStr, int] = {"user_id": user.id}
     return response_succ(body=payload)
 
-@api.route('/login', methods=['POST'])
+
+@api.route("/login", methods=["POST"])
 def login():
     params = parse_params(request)
     email: str = params.get("email")
     password: str = params.get("password")
-    exsist_user: User = session.query(User).filter_by(email=email, password=password).first()
+    exsist_user: User = session.query(User).filter_by(
+        email=email, password=password
+    ).first()
     if exsist_user:
         # update log time
         login_time: str = get_unix_time_tuple()
@@ -39,24 +43,30 @@ def login():
         record: LoginRecordModel = LoginRecordModel(exsist_user.id, log_ip)
         record.save()
         # update token
-        token: AnyStr = getmd5("-".join([email, password, login_time, get_random_num()]))
+        token: AnyStr = getmd5("-".join([email, password, get_random_num(2)]))
         # 保存到redis中, 设置有效时间为7天
-        redisClient.set(str(exsist_user.id), token, 60*60*24*7)
-        return response_succ(body={'token': token})
+        redisClient.set(exsist_user.get_cache_key(), token, 60 * 60 * 24 * 7)
+        payload: Dict[AnyStr, any] = {
+            "token": token,
+            "user_id": exsist_user.id
+        }
+        return response_succ(body=payload)
     else:
         return UserError.get_error(40203)
 
 
-@api.route('/logout', methods=['POST'])
+@api.route("/logout", methods=["POST"])
 def logout():
     """  登出
     设置redis时间为过期
     """
     pass
 
-@api.route('/info', methods=['GET'])
+
+@api.route("/info", methods=["GET"])
 @login_require
 def user_info():
     params = parse_params(request)
     user: User = get_current_user()
-
+    payload: Dict[AnyStr, any] = {"user_id": user.id}
+    return response_succ(body=payload)
